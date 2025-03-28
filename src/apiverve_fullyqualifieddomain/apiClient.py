5969d0542e2e106b38f6f368964df6ce4df59cb9
APIVerve Team <hello@apiverve.com>
2025-03-28 17:21:22 -0500

    Bug Fixes

--- src/apiverve_fullyqualifieddomain/apiClient.py
+++ src/apiverve_fullyqualifieddomain/apiClient.py
@@ -10,10 +10,10 @@ class FullyqualifieddomainAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/fullyqualifieddomain'
+        self.base_url = 'https://api.apiverve.com/v1/fullyqualifieddomain'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
